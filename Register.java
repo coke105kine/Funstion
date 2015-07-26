7a4b32d42902b8d6e82ac9ffc724f2d0d305a7fc
jli2011 <jli2011@users.noreply.github.com>
2015-07-25 22:08:27 -1000

    Create Register.java
    Register screen Java code

---
+++ Register.java
@@ -0,0 +1,38 @@
+package com.example.user.funstion;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends ActionBarActivity implements View.OnClickListener{
+
+    Button bRegister;
+    EditText etFirstName, etLastName, etEmail, etUsername, etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bRegister:
+                break;
+        }
+    }
+}
