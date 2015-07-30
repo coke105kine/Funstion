5e7649e530005f0d9da466197c5148d0dfcb6d1e
jli2011 <jli2011@users.noreply.github.com>
2015-07-30 01:05:04 -1000

    Create UserInfo.java

---
+++ UserInfo.java
@@ -0,0 +1,18 @@
+package com.example.user.funstion;
+
+/**
+ * Created by User on 7/29/2015.
+ */
+public class UserInfo {
+
+    public static abstract class NewUserInfo{
+
+        public static final String FIRST_NAME = "first_name";
+        public static final String LAST_NAME = "last_name";
+        public static final String EMAIL = "email";
+        public static final String USERNAME = "username";
+        public static final String PASSWORD = "password";
+        public static final String PASSWORD_CONFIRM = "password_confirm";
+        public static final String TABLE_NAME = "user_info";
+    }
+}
