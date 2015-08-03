cdc1e539910dc1a88d018dc47f2195a14f52b618
jli2011 <jli2011@users.noreply.github.com>
2015-08-02 20:41:15 -1000

    Create DataListActivity.java

---
+++ DataListActivity.java
@@ -0,0 +1,41 @@
+package com.example.user.funstion;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+public class DataListActivity extends ActionBarActivity {
+    ListView listView;
+    SQLiteDatabase sqLiteDatabase;
+    UserDbHelper userDbHelper;
+    Cursor cursor;
+    ListDataAdapter listDataAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_list_layout);
+        listView = (ListView)findViewById(R.id.list_view);
+        listDataAdapter = new ListDataAdapter(getApplicationContext(), R.layout.row_layout);
+        listView.setAdapter(listDataAdapter);
+        userDbHelper = new UserDbHelper(getApplicationContext());
+        sqLiteDatabase = userDbHelper.getReadableDatabase();
+        cursor = userDbHelper.getInformation(sqLiteDatabase);
+        //adds rows continuous
+        if (cursor.moveToFirst()) {
+            do {
+                String username, password, email;
+                username = cursor.getString(0);
+                password = cursor.getString(1);
+                email = cursor.getString(2);
+                DataProvider dataProvider = new DataProvider(username, password, email);
+                listDataAdapter.add(dataProvider);
+            } while (cursor.moveToNext()); //moves to next row
+        }
+    }
+}
