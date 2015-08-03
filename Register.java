bf53e7641b12181ead2b5074b5075596e2288802
jli2011 <jli2011@users.noreply.github.com>
2015-08-02 22:55:59 -1000

    Update Register.java
    Update register button

--- Register.java
+++ Register.java
@@ -3,7 +3,7 @@ package com.example.user.funstion;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivityy;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,10 +12,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Register extends AppCompatActivity implements View.OnClickListener{
+public class Register extends ActionBarActivity implements View.OnClickListener{
 
     Button bRegister;
-    EditText etFirstName, etLastName, etEmail, etUsername, etPassword, etConfirmPassword;
+    EditText etFirstName, etLastName, etEmail, etUsername, etPassword, etPasswordConfirm;
     Context context = this;
     UserDbHelper userDbHelper;
     SQLiteDatabase sqLiteDatabase;
@@ -30,9 +30,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         etEmail = (EditText) findViewById(R.id.etEmail);
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
-        etConfirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        etPasswordConfirm = (EditText) findViewById(R.id.etConfirmPassword);
         bRegister = (Button) findViewById(R.id.bRegister);
-
         bRegister.setOnClickListener(this);
     }
 
@@ -42,7 +41,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         String email = etEmail.getText().toString();
         String username = etUsername.getText().toString();
         String password = etPassword.getText().toString();
-        String passwordConfirm = etConfirmPassword.getText().toString();
+        String passwordConfirm = etPasswordConfirm.getText().toString();
         userDbHelper = new UserDbHelper(context);
         sqLiteDatabase = userDbHelper.getWritableDatabase();
         userDbHelper.addInformation(firstName, lastName, email, username, password, passwordConfirm, sqLiteDatabase);
@@ -58,9 +57,14 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.bRegister:// Send new user info to database
-                startActivity(new Intent(this, DataListActivity.class));
-                break;
+            case R.id.bRegister: //when register button is clicked
+                String firstName = etFirstName.getText().toString();
+                String lastName = etLastName.getText().toString();
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String passwordConfirm = etPasswordConfirm.getText().toString();
+                String email = etEmail.getText().toString();
+                User registeredData = new User(firstName, lastName, username, password, passwordConfirm, email);
                 break;
         }
     }
